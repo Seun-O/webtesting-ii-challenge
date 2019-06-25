da6653edec2ac9ef89e191992ec53d6ce6ce98a9
Seun-O <12373359+Seun-O@users.noreply.github.com>
2019-06-24 20:42:21 -0400

    Project Setup

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+// import ReactDOM from "react-dom";
+import { render, cleanup } from "@testing-library/react";
+import "jest-dom/extend-expect";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(cleanup);
+
+// it("renders without crashing", () => {
+//   const div = document.createElement("div");
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+it("renders without crashing ", () => {
+  render(<App />);
 });
---
+++ client/src/components/Dashboard/Dashboard.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "jest-dom/extend-expect";
+
+import Dashboard from "./Dashboard";
+
+afterEach(cleanup);
+
+it("renders without crashing ", () => {
+  render(<Dashboard />);
+});
